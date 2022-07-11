ca0409ed4ad2c307164e0cdc887568ba08fcea69
ljayG <moguri61@gmail.com>
2022-07-12 00:35:17 +0900

    신청서 선택하기 페이지 업로드

--- src/front/resources/js/love.js
+++ src/front/resources/js/love.js
@@ -8,4 +8,22 @@ $(document).ready(function () {
     scrolllock: true,
     transition: "all 0.3s",
   });
+
+  $("#btnListMode").bind("click", function (e) {
+    e.preventDefault();
+
+    let thText = $(this).children("span");
+
+    if (!$(this).hasClass("type_gallery")) {
+      thText.text("갤러리형 보기");
+      $(this).removeClass("type_list").addClass("type_gallery");
+      $("#typeGallery").removeClass("active");
+      $("#typeList").addClass("active");
+    } else if (!$(this).hasClass("type_list")) {
+      thText.text("리스트형 보기");
+      $(this).removeClass("type_gallery").addClass("type_list");
+      $("#typeList").removeClass("active");
+      $("#typeGallery").addClass("active");
+    }
+  });
 });
